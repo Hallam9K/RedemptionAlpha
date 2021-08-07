87121c15a3442a240704294edcc32a9bae97d01d
Hallam9K <32880148+Hallam9K@users.noreply.github.com>
2021-08-07 13:28:04 +0100

    dont work yet

--- Redemption.cs
+++ Redemption.cs
@@ -1,17 +1,134 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using Redemption.Globals;
+using Redemption.StructureHelper;
+using Redemption.StructureHelper.ChestHelper.GUI;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.DataStructures;
 using Terraria.ModLoader;
+using Terraria.UI;
 
 namespace Redemption
 {
-	public class Redemption : Mod
-	{
-		public override void AddRecipes() => RecipeSystem.Load(this);
+    public class Redemption : Mod
+    {
+        public override void AddRecipes() => RecipeSystem.Load(this);
 
-		public static Redemption Instance { get; private set; }
+        public static Redemption Instance { get; private set; }
 
-		public Redemption()
-		{
-			Instance = this;
-		}
+        public Redemption()
+        {
+            Instance = this;
+        }
+    }
+    public class RedeSystem : ModSystem
+    {
+        UserInterface GeneratorMenuUI;
+        internal ManualGeneratorMenu GeneratorMenu;
+
+        UserInterface ChestMenuUI;
+        internal ChestCustomizerState ChestCustomizer;
+
+        public override void Load()
+        {
+            if (!Main.dedServ)
+            {
+                GeneratorMenuUI = new UserInterface();
+                GeneratorMenu = new ManualGeneratorMenu();
+                GeneratorMenuUI.SetState(GeneratorMenu);
+
+                ChestMenuUI = new UserInterface();
+                ChestCustomizer = new ChestCustomizerState();
+                ChestMenuUI.SetState(ChestCustomizer);
+            }
+        }
+        public override void ModifyInterfaceLayers(List<GameInterfaceLayer> layers)
+        {
+            layers.Insert(layers.FindIndex(layer => layer.Name.Equals("Vanilla: Mouse Text")), new LegacyGameInterfaceLayer("GUI Menus",
+                delegate
+                {
+                    if (ManualGeneratorMenu.Visible)
+                    {
+                        GeneratorMenuUI.Update(Main._drawInterfaceGameTime);
+                        GeneratorMenu.Draw(Main.spriteBatch);
+                    }
+
+                    if (ChestCustomizerState.Visible)
+                    {
+                        ChestMenuUI.Update(Main._drawInterfaceGameTime);
+                        ChestCustomizer.Draw(Main.spriteBatch);
+                    }
+
+                    return true;
+                }, InterfaceScaleType.UI));
+        }
+        #region StructureHelper Draw
+        public override void PostDrawInterface(SpriteBatch spriteBatch)
+        {
+            if (Main.LocalPlayer.HeldItem.ModItem is CopyWand)
+            {
+                spriteBatch.End();
+                spriteBatch.Begin(default, default, default, default, default, default, Main.GameViewMatrix.ZoomMatrix);
+
+                Texture2D tex = (Texture2D)ModContent.Request<Texture2D>("Redemption/StructureHelper/corner");
+                Texture2D tex2 = (Texture2D)ModContent.Request<Texture2D>("Redemption/StructureHelper/box1");
+                Point16 TopLeft = (Main.LocalPlayer.HeldItem.ModItem as CopyWand).TopLeft;
+                int Width = (Main.LocalPlayer.HeldItem.ModItem as CopyWand).Width;
+                int Height = (Main.LocalPlayer.HeldItem.ModItem as CopyWand).Height;
+
+                float tileScale = 16 * Main.GameViewMatrix.Zoom.Length() * 0.707106688737f;
+                Vector2 pos = (Main.MouseWorld / tileScale).ToPoint16().ToVector2() * tileScale - Main.screenPosition;
+                pos = Vector2.Transform(pos, Matrix.Invert(Main.GameViewMatrix.ZoomMatrix));
+                pos = Vector2.Transform(pos, Main.UIScaleMatrix);
+
+                spriteBatch.Draw(tex, pos, tex.Frame(), Color.White * 0.5f, 0, tex.Frame().Size() / 2, 1, 0, 0);
+
+                if (Width != 0 && TopLeft != null)
+                {
+                    spriteBatch.Draw(tex2, new Rectangle((int)(TopLeft.X * 16 - Main.screenPosition.X), (int)(TopLeft.Y * 16 - Main.screenPosition.Y), Width * 16 + 16, Height * 16 + 16), tex2.Frame(), Color.White * 0.15f);
+                    spriteBatch.Draw(tex, (TopLeft.ToVector2() + new Vector2(Width + 1, Height + 1)) * 16 - Main.screenPosition, tex.Frame(), Color.Red, 0, tex.Frame().Size() / 2, 1, 0, 0);
+                }
+                if (TopLeft != null) spriteBatch.Draw(tex, TopLeft.ToVector2() * 16 - Main.screenPosition, tex.Frame(), Color.Cyan, 0, tex.Frame().Size() / 2, 1, 0, 0);
+
+                spriteBatch.End();
+                spriteBatch.Begin(default, default, default, default, default, default, Main.UIScaleMatrix);
+            }
+
+            if (Main.LocalPlayer.HeldItem.ModItem is MultiWand)
+            {
+                spriteBatch.End();
+                spriteBatch.Begin(default, default, default, default, default, default, Main.GameViewMatrix.ZoomMatrix);
+
+                Texture2D tex = (Texture2D)ModContent.Request<Texture2D>("Redemption/StructureHelper/corner");
+                Texture2D tex2 = (Texture2D)ModContent.Request<Texture2D>("Redemption/StructureHelper/box1");
+                Point16 TopLeft = (Main.LocalPlayer.HeldItem.ModItem as MultiWand).TopLeft;
+                int Width = (Main.LocalPlayer.HeldItem.ModItem as MultiWand).Width;
+                int Height = (Main.LocalPlayer.HeldItem.ModItem as MultiWand).Height;
+                int count = (Main.LocalPlayer.HeldItem.ModItem as MultiWand).StructureCache.Count;
+
+                float tileScale = 16 * Main.GameViewMatrix.Zoom.Length() * 0.707106688737f;
+                Vector2 pos = (Main.MouseWorld / tileScale).ToPoint16().ToVector2() * tileScale - Main.screenPosition;
+                pos = Vector2.Transform(pos, Matrix.Invert(Main.GameViewMatrix.ZoomMatrix));
+                pos = Vector2.Transform(pos, Main.UIScaleMatrix);
+
+                spriteBatch.Draw(tex, pos, tex.Frame(), Color.White * 0.5f, 0, tex.Frame().Size() / 2, 1, 0, 0);
+
+                if (Width != 0 && TopLeft != null)
+                {
+                    spriteBatch.Draw(tex2, new Rectangle((int)(TopLeft.X * 16 - Main.screenPosition.X), (int)(TopLeft.Y * 16 - Main.screenPosition.Y), Width * 16 + 16, Height * 16 + 16), tex2.Frame(), Color.White * 0.15f);
+                    spriteBatch.Draw(tex, (TopLeft.ToVector2() + new Vector2(Width + 1, Height + 1)) * 16 - Main.screenPosition, tex.Frame(), Color.Yellow, 0, tex.Frame().Size() / 2, 1, 0, 0);
+                }
+                if (TopLeft != null) spriteBatch.Draw(tex, TopLeft.ToVector2() * 16 - Main.screenPosition, tex.Frame(), Color.LimeGreen, 0, tex.Frame().Size() / 2, 1, 0, 0);
+
+                spriteBatch.End();
+                spriteBatch.Begin();
+                Utils.DrawBorderString(spriteBatch, "Structures to save: " + count, Main.MouseScreen + new Vector2(0, 30), Color.White);
+
+                spriteBatch.End();
+                spriteBatch.Begin(default, default, default, default, default, default, Main.UIScaleMatrix);
+            }
+        }
+        #endregion
     }
 }
---
+++ StructureHelper/Saver.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+
+namespace Redemption.StructureHelper
+{
+	internal static class Saver
+	{
+        public static void SaveToFile(Rectangle target, string targetPath = null)
+		{
+            string path = ModLoader.ModPath.Replace("Mods", "SavedStructures");
+
+            if (!Directory.Exists(path)) 
+                Directory.CreateDirectory(path);
+
+            string thisPath = targetPath ?? Path.Combine(path, "SavedStructure_" + DateTime.Now.ToString("d-M-y----H-m-s-f"));
+
+            Main.NewText("Structure saved as " + thisPath, Color.Yellow);
+            FileStream stream = File.Create(thisPath);
+            stream.Close();
+
+            var tag = SaveStructure(target);
+            TagIO.ToFile(tag, thisPath);
+        }
+
+        public static void SaveMultistructureToFile(ref List<TagCompound> toSave, string targetPath = null)
+		{
+            string path = ModLoader.ModPath.Replace("Mods", "SavedStructures");
+
+            if (!Directory.Exists(path)) 
+                Directory.CreateDirectory(path);
+
+            string thisPath = targetPath ?? Path.Combine(path, "SavedMultiStructure_" + DateTime.Now.ToString("d-M-y----H-m-s-f"));
+
+            Main.NewText("Structure saved as " + thisPath, Color.Yellow);
+            FileStream stream = File.Create(thisPath);
+            stream.Close();
+
+            TagCompound tag = new TagCompound();
+            tag.Add("Structures", toSave);
+            tag.Add("Version", Redemption.Instance.Version.ToString());
+
+            TagIO.ToFile(tag, thisPath);
+
+            toSave.Clear();
+        }
+
+        public static TagCompound SaveStructure(Rectangle target)
+        {
+            TagCompound tag = new TagCompound();
+            tag.Add("Version", Redemption.Instance.Version.ToString());
+            tag.Add("Width", target.Width);
+            tag.Add("Height", target.Height);
+
+            List<TileSaveData> data = new List<TileSaveData>();
+            for (int x = target.X; x <= target.X + target.Width; x++)
+            {
+                for (int y = target.Y; y <= target.Y + target.Height; y++)
+                {
+                    Tile tile = Framing.GetTileSafely(x, y);
+                    string tileName;
+                    string wallName;
+                    string teName;
+                    if (tile.type >= TileID.Count) tileName = ModContent.GetModTile(tile.type).Mod.Name + " " + ModContent.GetModTile(tile.type).Name;
+                    else tileName = tile.type.ToString();
+                    if (tile.wall >= WallID.Count) wallName = ModContent.GetModWall(tile.wall).Mod.Name + " " + ModContent.GetModWall(tile.wall).Name;
+                    else wallName = tile.wall.ToString();
+
+                    TileEntity teTarget = null; //grabbing TE data
+                    TagCompound entityTag = null;
+
+                    if (TileEntity.ByPosition.ContainsKey(new Point16(x, y))) teTarget = TileEntity.ByPosition[new Point16(x, y)];
+
+                    if (teTarget != null)
+                    {
+                        if (teTarget.type < 2)
+                        {
+                            teName = teTarget.type.ToString();
+                        }
+                        else
+                        {
+                            ModTileEntity entityTarget = (ModTileEntity)TileEntity.manager.GetTileEntity<ModTileEntity>(teTarget.type);
+                            if (entityTarget != null)
+                            {
+                                teName = entityTarget.Mod.Name + " " + entityTarget.Name;
+                                entityTag = (teTarget as ModTileEntity).Save();
+                            }
+                            else teName = "";
+                        }
+                    }
+                    else teName = "";
+
+                    data.Add(
+                        new TileSaveData(
+                            tileName,
+                            wallName,
+                            tile.frameX, 
+                            tile.frameY,
+
+                            tile.bTileHeader,
+                            tile.bTileHeader2,
+                            tile.bTileHeader3,
+                            tile.sTileHeader,
+
+                            teName,
+                            entityTag
+                            ));
+                }
+            }
+
+            tag.Add("TileData", data);
+            return tag;
+        }
+    }
+}
