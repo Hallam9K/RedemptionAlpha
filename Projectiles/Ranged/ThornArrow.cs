ade13a09688d0c1333b1c1a73e6a44a6ed7efb50
Hallam <hallam@codepagesystems.com>
2021-09-24 16:43:30 +0100

    thorny arrwo

---
+++ Projectiles/Ranged/ThornArrow.cs
@@ -0,0 +1,102 @@
+using Microsoft.Xna.Framework;
+using Redemption.Base;
+using Redemption.Dusts;
+using Redemption.Globals;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Redemption.Projectiles.Ranged
+{
+    public class ThornArrow : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Seed-Laden Arrow");
+            Main.projFrames[Projectile.type] = 5;
+        }
+        public override void SetDefaults()
+        {
+            Projectile.width = 14;
+            Projectile.height = 14;
+            Projectile.friendly = true;
+            Projectile.DamageType = DamageClass.Ranged;
+            Projectile.penetrate = 1;
+            Projectile.timeLeft = 600;
+            Projectile.extraUpdates = 1;
+        }
+        public override void Kill(int timeLeft)
+        {
+            Player player = Main.player[Projectile.owner];
+            if (Main.myPlayer == player.whoAmI)
+                Projectile.NewProjectile(Projectile.InheritSource(Projectile), Projectile.Center, Vector2.Zero, ModContent.ProjectileType<ThornArrowSeed>(), Projectile.damage, Projectile.knockBack, Projectile.owner);
+        }
+        public override void AI()
+        {
+            if (++Projectile.frameCounter >= 5)
+            {
+                Projectile.frameCounter = 0;
+                if (++Projectile.frame >= 5)
+                    Projectile.frame = 0;
+            }
+            Projectile.rotation = Projectile.velocity.ToRotation() + MathHelper.PiOver2;
+            Projectile.velocity.Y += 0.05f;
+        }
+        public override bool OnTileCollide(Vector2 oldVelocity)
+        {
+            Collision.HitTiles(Projectile.position, oldVelocity, Projectile.width, Projectile.height);
+            return true;
+        }
+    }
+    public class ThornArrowSeed : ModProjectile
+    {
+        private bool spawnDust;
+
+        public override string Texture => "Redemption/NPCs/Bosses/Thorn/ThornSeed";
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Thorn Seed");
+        }
+        public override void SetDefaults()
+        {
+            Projectile.width = 6;
+            Projectile.height = 6;
+            Projectile.penetrate = -1;
+            Projectile.hostile = false;
+            Projectile.friendly = false;
+            Projectile.tileCollide = true;
+            Projectile.ignoreWater = true;
+            Projectile.timeLeft = 120;
+            Projectile.alpha = 0;
+            Projectile.GetGlobalProjectile<RedeGlobalProjectile>().Unparryable = true;
+        }
+        public override bool TileCollideStyle(ref int width, ref int height, ref bool fallThrough, ref Vector2 hitboxCenterFrac)
+        {
+            fallThrough = false;
+            return true;
+        }
+        public override void AI()
+        {
+            Projectile.velocity.Y += 0.45f;
+            if (!spawnDust)
+            {
+                for (int m = 0; m < 8; m++)
+                {
+                    int dustID = Dust.NewDust(new Vector2(Projectile.Center.X - 1, Projectile.Center.Y - 1), 2, 2, DustID.Grass, 0f, 0f, 100, Color.White, 1.6f);
+                    Main.dust[dustID].velocity = BaseUtility.RotateVector(default, new Vector2(6f, 0f), m / (float)8 * 6.28f);
+                    Main.dust[dustID].noLight = false;
+                    Main.dust[dustID].noGravity = true;
+                }
+                spawnDust = true;
+            }
+        }
+        public override bool OnTileCollide(Vector2 oldVelocity)
+        {
+            Player player = Main.player[Projectile.owner];
+            if (Main.myPlayer == player.whoAmI)
+                Projectile.NewProjectile(Projectile.InheritSource(Projectile), new Vector2(Projectile.Center.X, Projectile.Center.Y - 36), Vector2.Zero, ModContent.ProjectileType<ThornTrapSmall_Proj>(), Projectile.damage, 3, Main.myPlayer);
+
+            return true;
+        }
+    }
+}
