9803fd9930f0d0033ee5d8051bd086e9ab9e48ec
Sneaklone <mattiastforsberg@gmail.com>
2021-10-13 03:02:10 -0600

    shade armour except sheeting sucks!

---
+++ Items/Armor/PostML/ShadeBody.cs
@@ -0,0 +1,40 @@
+using Microsoft.Xna.Framework;
+using Redemption.DamageClasses;
+using Redemption.Globals.Player;
+using Redemption.Rarities;
+using Terraria;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Redemption.Items.Armor.PostML
+{
+    [AutoloadEquip(EquipType.Body)]
+    public class ShadeBody : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Shadeplate");
+            Tooltip.SetDefault("10% increased ritual damage\n" +
+                "\n[c/bdffff:Spirit Level +2]");
+
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 38;
+            Item.height = 32;
+            Item.sellPrice(7, 50, 0);
+            Item.rare = ModContent.RarityType<SoullessRarity>();
+            Item.defense = 32;
+        }
+
+        public override void UpdateEquip(Player player)
+        {
+            player.GetDamage<RitualistClass>() += .10f;
+            RedePlayer modPlayer = player.GetModPlayer<RedePlayer>();
+            modPlayer.maxSpiritLevel += 2;
+        }
+    }
+}
