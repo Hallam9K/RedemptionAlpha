eb34abd837e566be4e0dabb2a0e0d49402ef4afe
Hallam9K <32880148+Hallam9K@users.noreply.github.com>
2021-10-25 14:16:32 +0100

    pouch belt

---
+++ Items/Accessories/PreHM/PouchBelt.cs
@@ -0,0 +1,39 @@
+﻿using Redemption.DamageClasses;
+using Redemption.Globals.Player;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Redemption.Items.Accessories.PreHM
+{
+    public class PouchBelt : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+            DisplayName.SetDefault("Pouch Belt");
+            Tooltip.SetDefault("+2 druidic damage"
+                + "\n2% increased druidic critical strike chance");
+        }
+
+		public override void SetDefaults()
+		{
+            Item.width = 30;
+            Item.height = 24;
+            Item.value = Item.sellPrice(0, 0, 15, 0);
+            Item.rare = ItemRarityID.Blue;
+            Item.accessory = true;
+        }
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ItemID.Leather, 4)
+                .AddTile(TileID.WorkBenches)
+                .Register();
+        }
+        public override void UpdateAccessory(Player player, bool hideVisual)
+		{
+            player.GetModPlayer<BuffPlayer>().DruidDamageFlat += 2;
+            player.GetCritChance<DruidClass>() += 2;
+		}
+	}
+}
