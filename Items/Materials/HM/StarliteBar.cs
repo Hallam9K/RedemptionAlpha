396faf8e5dc46162d281941f1295771a7970923f
Moonseer12 <79872729+Moonseer12@users.noreply.github.com>
2021-11-10 16:57:49 -0700

    Added starlite bar

---
+++ Items/Materials/HM/StarliteBar.cs
@@ -0,0 +1,39 @@
+using Redemption.Items.Materials.HM;
+using Redemption.Tiles.Bars;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Redemption.Items.Materials.HM
+{
+	public class StarliteBar : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Starlite Bar");
+			Tooltip.SetDefault("'Man-made metal that was made a long time ago, but recipe was lost for hundreds of years...'");
+		}
+		public override void SetDefaults()
+		{
+			Item.width = 30;
+			Item.height = 24;
+			Item.maxStack = 99;
+			Item.value = Item.sellPrice(0, 0, 10, 0);
+			Item.rare = ItemRarityID.Lime;
+			Item.useTurn = true;
+			Item.autoReuse = true;
+			Item.useAnimation = 15;
+			Item.useTime = 10;
+			Item.useStyle = 1;
+			Item.consumable = true;
+			Item.createTile = ModContent.TileType<StarliteBarTile>();
+		}
+		public override void AddRecipes()
+		{
+			CreateRecipe()
+			.AddIngredient(ModContent.ItemType<Starlite>(), 2)
+			.AddTile(TileID.AdamantiteForge)
+			.Register();
+		}
+	}
+}
---
+++ Tiles/Bars/StarliteBarTile.cs
@@ -0,0 +1,31 @@
+using Microsoft.Xna.Framework;
+using Redemption.Items.Materials.HM;
+using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace Redemption.Tiles.Bars
+{
+	public class StarliteBarTile : ModTile
+	{
+		public override void SetStaticDefaults()
+		{
+			SoundType = SoundID.Tink;
+			Main.tileShine[Type] = 1100;
+			Main.tileSolid[Type] = true;
+			Main.tileSolidTop[Type] = true;
+			Main.tileFrameImportant[Type] = true;
+            TileID.Sets.IgnoredByNpcStepUp[Type] = true;
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style1x1);
+			TileObjectData.newTile.StyleHorizontal = true;
+			TileObjectData.newTile.LavaDeath = false;
+			TileObjectData.addTile(Type);
+			ItemDrop = ModContent.ItemType<StarliteBar>();
+            DustType = DustID.GreenTorch;
+            AddMapEntry(new Color(40, 200, 100));
+			MinPick = 0;
+		}
+	}
+}
