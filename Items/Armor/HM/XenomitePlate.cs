70e64d3032fac1932cb363d45f01380f9ce0e7e0
Sneaklone <mattiastforsberg@gmail.com>
2021-11-11 19:59:30 -0700

    xen

---
+++ Items/Armor/HM/XenomitePlate.cs
@@ -0,0 +1,46 @@
+using Microsoft.Xna.Framework;
+using Redemption.Items.Materials.HM;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Redemption.Items.Armor.HM
+{
+    [AutoloadEquip(EquipType.Body)]
+    public class XenomitePlate : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Tooltip.SetDefault("5% increased damage\n" +
+            "4% increased critical strike chance");
+
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+        }
+
+        public override void UpdateEquip(Player player)
+        {
+            player.GetDamage(DamageClass.Generic) += .05f;
+            player.GetCritChance(DamageClass.Generic) += 4;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 42;
+            Item.height = 24;
+            Item.sellPrice(silver:50);
+            Item.rare = ItemRarityID.Lime;
+            Item.defense = 12;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ModContent.ItemType<Starlite>(), 8)
+                .AddIngredient(ModContent.ItemType<Xenomite>(), 20)
+                .AddTile(TileID.MythrilAnvil)
+                .Register();
+        }
+    }
+}
