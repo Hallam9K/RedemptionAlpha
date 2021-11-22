1dfd5c5df670b9f15f289d17fb1fb5cbf4ca4248
Moonseer12 <79872729+Moonseer12@users.noreply.github.com>
2021-11-21 20:14:26 -0700

    More cleanup

--- Items/Materials/PostML/RawXenium.cs
+++ Items/Materials/PostML/RawXenium.cs
@@ -1,37 +1,37 @@
-using Redemption.Items.Materials.HM;
-using Redemption.Items.Materials.PreHM;
-using Redemption.Tiles.Furniture.Lab;
-using Terraria;
-using Terraria.GameContent.Creative;
 using Terraria.ID;
+using Terraria;
 using Terraria.ModLoader;
+using Terraria.GameContent.Creative;
+using Redemption.Items.Materials.PreHM;
+using Redemption.Items.Materials.HM;
+using Redemption.Tiles.Furniture.Lab;
 
 namespace Redemption.Items.Materials.PostML
 {
-	public class RawXenium : ModItem
-	{
-		public override void SetStaticDefaults()
-		{
-			DisplayName.SetDefault("Raw Xenium");
-			Tooltip.SetDefault("Use a Xenium Refinery to craft Xenium Bars");
+    public class RawXenium : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Tooltip.SetDefault("Use a Xenium Refinery to craft Xenium Bars");
             CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 50;
         }
-		public override void SetDefaults()
-		{
-			Item.width = 20;
-			Item.height = 20;
-			Item.maxStack = 999;
-			Item.value = Item.sellPrice(0, 0, 15, 0);
-			Item.rare = ItemRarityID.Purple;
+
+        public override void SetDefaults()
+        {
+            Item.width = 20;
+            Item.height = 20;
+            Item.maxStack = 999;
+            Item.value = Item.sellPrice(0, 0, 15, 0);
+            Item.rare = ItemRarityID.Purple;
         }
-		public override void AddRecipes()
-		{
-			CreateRecipe(2)
-			.AddIngredient(ModContent.ItemType<XenomiteShard>(), 2)
-			.AddIngredient(ModContent.ItemType<Starlite>(), 2)
-			.AddIngredient(ItemID.LunarOre, 1)
-			.AddTile(ModContent.TileType<XenoTank1>())
-			.Register();
-		}
-	}
+        public override void AddRecipes()
+        {
+            CreateRecipe(2)
+                .AddIngredient(ModContent.ItemType<XenomiteShard>(), 2)
+                .AddIngredient(ModContent.ItemType<StarliteBar>(), 2)
+                .AddIngredient(ItemID.LunarOre)
+                .AddTile(ModContent.TileType<XeniumRefineryTile>())
+                .Register();
+        }
+    }
 }
