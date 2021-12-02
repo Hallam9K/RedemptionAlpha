97513e1f611c3e952c9cc33d94653c356b2df4d3
Hallam9K <32880148+Hallam9K@users.noreply.github.com>
2021-12-02 23:10:43 +0000

    Update IrradiatedBehemoth.cs

--- NPCs/Lab/Behemoth/IrradiatedBehemoth.cs
+++ NPCs/Lab/Behemoth/IrradiatedBehemoth.cs
@@ -218,13 +218,8 @@ namespace Redemption.NPCs.Lab.Behemoth
                     }
                     break;
             }
-            if (Main.LocalPlayer.Center.Y < NPC.Center.Y)
-            {
-                if (Main.rand.NextBool(5))
-                {
-                    NPC.Shoot(new Vector2(NPC.Center.X + Main.rand.Next(0, NPC.width), NPC.Center.Y), ModContent.ProjectileType<GreenGas_Proj>(), 200, new Vector2(0, Main.rand.Next(-20, -10)), false, SoundID.Item1.WithVolume(0));
-                }
-            }
+            if (Main.LocalPlayer.Center.Y < NPC.Center.Y && Main.rand.NextBool(5))
+                NPC.Shoot(new Vector2(NPC.Center.X + Main.rand.Next(0, NPC.width), NPC.Center.Y), ModContent.ProjectileType<GreenGas_Proj>(), 200, new Vector2(0, Main.rand.Next(-20, -10)), false, SoundID.Item1.WithVolume(0));
         }
         private int AniFrameY;
         private Vector2 HandVector;
