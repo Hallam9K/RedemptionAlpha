667cad80bd004a05400ca76cc304b20eb700bdc9
Hallam9K <32880148+Hallam9K@users.noreply.github.com>
2022-01-06 15:51:57 +0000

    wasteland campfire

---
+++ Tiles/Furniture/Misc/TrojanChickenTile.cs
@@ -0,0 +1,48 @@
+using Microsoft.Xna.Framework;
+using Redemption.Dusts.Tiles;
+using Redemption.Items.Placeable.Furniture.Lab;
+using Redemption.Items.Placeable.Furniture.Misc;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.Enums;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace Redemption.Tiles.Furniture.Misc
+{
+    public class TrojanChickenTile : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.tileFrameImportant[Type] = true;
+            Main.tileLavaDeath[Type] = false;
+            Main.tileNoAttach[Type] = true;
+            TileObjectData.newTile.Width = 11;
+            TileObjectData.newTile.Height = 7;
+            TileObjectData.newTile.CoordinateHeights = new int[] { 16, 16, 16, 16, 16, 16, 16 };
+            TileObjectData.newTile.Origin = new Point16(4, 6);
+            TileObjectData.newTile.UsesCustomCanPlace = true;
+            TileObjectData.newTile.StyleHorizontal = true;
+            TileObjectData.newTile.Direction = TileObjectDirection.PlaceLeft;
+            TileObjectData.newTile.CoordinateWidth = 16;
+            TileObjectData.newTile.CoordinatePadding = 2;
+            TileObjectData.newTile.AnchorBottom = new AnchorData(AnchorType.SolidTile | AnchorType.SolidWithTop | AnchorType.SolidSide, TileObjectData.newTile.Width, 0);
+            TileObjectData.newAlternate.CopyFrom(TileObjectData.newTile);
+            TileObjectData.newAlternate.Direction = TileObjectDirection.PlaceRight;
+            TileObjectData.addAlternate(1);
+            TileObjectData.addTile(Type);
+            DustType = DustID.WoodFurniture;
+            MinPick = 10;
+            MineResist = 2f;
+            ModTranslation name = CreateMapEntryName();
+            name.SetDefault("Trojan Chicken Replica");
+            AddMapEntry(new Color(151, 107, 75), name);
+        }
+        public override void KillMultiTile(int i, int j, int frameX, int frameY)
+        {
+            Item.NewItem(i * 16, j * 16, 176, 112, ModContent.ItemType<TrojanChicken>());
+        }
+        public override bool CanExplode(int i, int j) => false;
+    }
+}
