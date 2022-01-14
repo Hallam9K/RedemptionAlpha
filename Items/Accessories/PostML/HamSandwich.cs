e5e67ee18ca1a3b5d6756877f0cdcc71cf379f60
Hallam9K <32880148+Hallam9K@users.noreply.github.com>
2022-01-14 17:24:25 +0000

    smallam

---
+++ Items/Accessories/PostML/HamSandwich.cs
@@ -0,0 +1,36 @@
+using Microsoft.Xna.Framework;
+using Redemption.Buffs.Pets;
+using Redemption.Projectiles.Pets;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Redemption.Items.Accessories.PostML
+{
+    public class HamSandwich : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Ham Sandwich");
+            Tooltip.SetDefault("'Unleash doomsday upon this fragile universe'" +
+                "\nSummons !!??");
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.DefaultToVanitypet(ModContent.ProjectileType<HalPet>(), ModContent.BuffType<HalPetBuff>());
+            Item.width = 24;
+            Item.height = 20;
+            Item.rare = ItemRarityID.Expert;
+            Item.value = Item.sellPrice(0, 5);
+        }
+        public override bool Shoot(Player player, ProjectileSource_Item_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            player.AddBuff(Item.buffType, 2);
+            return false;
+        }
+    }
+}
