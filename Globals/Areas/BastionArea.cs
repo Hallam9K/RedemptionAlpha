859065bc6a25616af350d3ac1009afe90c82645e
Hallam9K <32880148+Hallam9K@users.noreply.github.com>
2022-04-01 11:10:36 +0100

    Update BastionArea.cs

--- Globals/Areas/BastionArea.cs
+++ Globals/Areas/BastionArea.cs
@@ -12,6 +12,7 @@ using System.Collections.Generic;
 using System.IO;
 using Terraria;
 using Terraria.DataStructures;
+using Terraria.ID;
 using Terraria.ModLoader;
 using Terraria.ModLoader.IO;
 
@@ -26,7 +27,7 @@ namespace Redemption.Globals
         }
         public override void PreUpdateWorld()
         {
-            if (!Active || RedeGen.BastionVector.X == -1 || RedeGen.BastionVector.Y == -1)
+            if (!Active || RedeGen.BastionVector.X == -1 || RedeGen.BastionVector.Y == -1 || Main.netMode == NetmodeID.MultiplayerClient)
                 return;
 
             Vector2 NozaPos = new((RedeGen.BastionVector.X + 210) * 16, (RedeGen.BastionVector.Y + 64) * 16);
