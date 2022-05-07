3e262ae46c926d07f1d096eff9c7c069f061e894
Hallam9K <32880148+Hallam9K@users.noreply.github.com>
2022-05-07 23:57:46 +0100

    big man

---
+++ NPCs/Soulless/RuhRoh.cs
@@ -0,0 +1,112 @@
+using CollisionLib;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Redemption.BaseExtension;
+using Redemption.Globals;
+using Terraria;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Redemption.NPCs.Soulless
+{
+    public class RuhRoh : ModNPC
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("");
+            NPCID.Sets.DebuffImmunitySets.Add(Type, new NPCDebuffImmunityData { ImmuneToAllBuffsThatAreNotWhips = true });
+            NPCID.Sets.NPCBestiaryDrawModifiers value = new(0) { Hide = true };
+            NPCID.Sets.NPCBestiaryDrawOffset.Add(Type, value);
+        }
+        public CollisionSurface[] colliders = null;
+        public override void SetDefaults()
+        {
+            NPC.width = 118;
+            NPC.height = 320;
+            NPC.lifeMax = 1000;
+            NPC.damage = 100;
+            NPC.immortal = true;
+            NPC.dontTakeDamage = true;
+            NPC.noGravity = true;
+            NPC.knockBackResist = 0;
+            NPC.aiStyle = -1;
+        }
+        public override bool CheckActive() => false;
+        public override bool PreAI()
+        {
+            if (colliders == null || colliders.Length != 1)
+            {
+                colliders = new CollisionSurface[] {
+                    new CollisionSurface(NPC.TopLeft, NPC.BottomLeft, new int[] { 1, 1, 1, 1 }, true) };
+            }
+            return true;
+        }
+        private bool Flare;
+        private float FlareTimer;
+        public override void AI()
+        {
+            Player player = Main.player[RedeHelper.GetNearestAlivePlayer(NPC)];
+            if (Flare)
+            {
+                FlareTimer++;
+                if (FlareTimer > 60)
+                {
+                    Flare = false;
+                    FlareTimer = 0;
+                }
+            }
+            switch (NPC.ai[0])
+            {
+                case 0:
+                    Rectangle sight = new((int)NPC.position.X - 200, (int)NPC.position.Y, 220, NPC.height);
+                    if (player.Hitbox.Intersects(sight) && !player.dead && player.active)
+                    {
+                        Flare = true;
+                        NPC.ai[0] = 1;
+                        NPC.netUpdate = true;
+                    }
+                    break;
+                case 1:
+                    NPC.velocity.X -= 0.01f;
+                    NPC.velocity.X = MathHelper.Clamp(NPC.velocity.X, -10, 0);
+                    break;
+            }
+
+            if (colliders != null && colliders.Length == 1)
+            {
+                colliders[0].Update();
+                colliders[0].endPoints[0] = NPC.Center + (NPC.TopLeft - NPC.Center).RotatedBy(NPC.rotation);
+                colliders[0].endPoints[1] = NPC.Center + (NPC.BottomLeft - NPC.Center).RotatedBy(NPC.rotation);
+            }
+        }
+        public override void PostDraw(SpriteBatch spriteBatch, Vector2 screenPos, Color drawColor)
+        {
+            spriteBatch.End();
+            spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.Additive, Main.DefaultSamplerState, DepthStencilState.None, RasterizerState.CullCounterClockwise, null, Main.GameViewMatrix.TransformationMatrix);
+
+            Texture2D flare = ModContent.Request<Texture2D>("Redemption/Textures/WhiteFlare").Value;
+            Rectangle rect = new(0, 0, flare.Width, flare.Height);
+            Vector2 origin = new(flare.Width / 2, flare.Height / 2);
+            Vector2 position = NPC.Center - screenPos + new Vector2(8 * NPC.spriteDirection, -10 + NPC.gfxOffY);
+            Color colour = Color.Lerp(Color.White, Color.White, 1f / FlareTimer * 10f) * (1f / FlareTimer * 10f);
+            if (Flare)
+            {
+                spriteBatch.Draw(flare, position, new Rectangle?(rect), colour, 0, origin, 1.3f, SpriteEffects.None, 0);
+                spriteBatch.Draw(flare, position, new Rectangle?(rect), colour * 0.4f, 0, origin, 1.3f, SpriteEffects.None, 0);
+            }
+            spriteBatch.End();
+            spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, Main.DefaultSamplerState, DepthStencilState.None, RasterizerState.CullCounterClockwise, null, Main.GameViewMatrix.TransformationMatrix);
+        }
+        public override void PostAI()
+        {
+            if (colliders != null)
+            {
+                foreach (CollisionSurface collider in colliders)
+                    collider.PostUpdate();
+            }
+        }
+    }
+}
