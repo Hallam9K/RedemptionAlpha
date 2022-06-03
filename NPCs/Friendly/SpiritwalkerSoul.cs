7befb8cc678127e1c4451c477523749d9240dde8
Hallam9K <32880148+Hallam9K@users.noreply.github.com>
2022-06-03 22:46:16 +0100

    spiritwalker start

---
+++ NPCs/Friendly/SpiritwalkerSoul.cs
@@ -0,0 +1,141 @@
+using Terraria;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
+using Terraria.ModLoader;
+using Redemption.Items.Materials.PreHM;
+using Redemption.Globals;
+using Terraria.Audio;
+using Terraria.GameContent.Bestiary;
+using Terraria.DataStructures;
+using Redemption.Particles;
+using ParticleLibrary;
+using Redemption.BaseExtension;
+using System;
+using Redemption.Dusts;
+
+namespace Redemption.NPCs.Friendly
+{
+    public class SpiritwalkerSoul : ModNPC
+    {
+        public override string Texture => "Redemption/NPCs/Friendly/LostSoulNPC";
+        public ref float AITimer => ref NPC.ai[0];
+        public ref float TimerRand => ref NPC.ai[1];
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Lost Soul");
+            NPCID.Sets.DebuffImmunitySets.Add(Type, new NPCDebuffImmunityData
+            {
+                ImmuneToAllBuffsThatAreNotWhips = true
+            });
+            NPCID.Sets.NPCBestiaryDrawModifiers value = new(0)
+            {
+                Hide = true
+            };
+            NPCID.Sets.NPCBestiaryDrawOffset.Add(Type, value);
+        }
+        public override void SetDefaults()
+        {
+            NPC.width = 10;
+            NPC.height = 10;
+            NPC.friendly = true;
+            NPC.dontTakeDamage = true;
+            NPC.immortal = true;
+            NPC.damage = 0;
+            NPC.defense = 0;
+            NPC.lifeMax = 1;
+            NPC.HitSound = SoundID.NPCHit36;
+            NPC.DeathSound = SoundID.NPCDeath39;
+            NPC.lavaImmune = true;
+            NPC.knockBackResist = 0f;
+            NPC.aiStyle = -1;
+            NPC.alpha = 200;
+            NPC.noGravity = true;
+            NPC.noTileCollide = true;
+        }
+        public override void AI()
+        {
+            int dust = Dust.NewDust(NPC.position + NPC.velocity, NPC.width, NPC.height, DustID.DungeonSpirit,
+                NPC.velocity.X * 0.5f, NPC.velocity.Y * 0.5f, Scale: 2);
+            Main.dust[dust].noGravity = true;
+            if (Main.rand.NextBool(3))
+                ParticleManager.NewParticle(NPC.Center, RedeHelper.Spread(2), new SpiritParticle(), Color.White, 1);
+
+            NPC.rotation = NPC.velocity.ToRotation() + MathHelper.Pi;
+            Player player = Main.player[RedeHelper.GetNearestAlivePlayer(NPC)];
+            RedeSystem.Silence = true;
+            switch (TimerRand)
+            {
+                case 0:
+                    player.RedemptionScreen().cutscene = true;
+                    player.RedemptionScreen().ScreenFocusPosition = NPC.Center;
+                    player.RedemptionScreen().lockScreen = true;
+
+                    NPC.velocity *= 0.98f;
+                    if (AITimer++ >= 120)
+                    {
+                        NPC.MoveToVector2(player.Center, 10);
+                        if (NPC.Hitbox.Intersects(player.Hitbox))
+                        {
+                            AITimer = 0;
+                            TimerRand = 1;
+                            NPC.velocity *= 0;
+                        }
+                    }
+                    break;
+                case 1:
+                    player.RedemptionScreen().cutscene = true;
+                    NPC.MoveToVector2(player.Center, 10);
+                    player.position = player.oldPosition;
+                    player.velocity *= 0;
+
+                    if (AITimer++ == 60)
+                        SoundEngine.PlaySound(CustomSounds.EnergyCharge, player.position);
+
+                    if (AITimer >= 60)
+                    {
+                        player.RedemptionScreen().ScreenShakeIntensity = AITimer / 30;
+                        for (int k = 0; k < 3; k++)
+                        {
+                            Vector2 vector;
+                            double angle = Main.rand.NextDouble() * 2d * Math.PI;
+                            vector.X = (float)(Math.Sin(angle) * 1000);
+                            vector.Y = (float)(Math.Cos(angle) * 1000);
+                            Dust dust2 = Main.dust[Dust.NewDust(player.Center + vector, 2, 2, ModContent.DustType<GlowDust>())];
+                            dust2.noGravity = true;
+                            dust2.noLight = true;
+                            Color dustColor = new(180, 255, 255) { A = 0 };
+                            dust2.color = dustColor;
+                            dust2.velocity = dust2.position.DirectionTo(player.Center) * 40f;
+                        }
+                        player.RedemptionAbility().SpiritwalkerActive = true;
+                    }
+                    if (AITimer >= 260)
+                    {
+                        string s = "Select a keybind for [Spirit Walker Key] in Controls";
+                        foreach (string key in Redemption.RedeSpiritwalkerAbility.GetAssignedKeys())
+                        {
+                            s = "Hold " + key + " to enter the Spirit Realm";
+                        }
+                        RedeSystem.Instance.TitleCardUIElement.DisplayTitle("-Spirit Walker-", 120, 90, 1f, 0, Color.White, s);
+
+                        SoundEngine.PlaySound(CustomSounds.NewLocation, player.position);
+                        player.RedemptionAbility().Spiritwalker = true;
+                        player.RedemptionAbility().SpiritwalkerActive = false;
+
+                        for (int i = 0; i < 20; i++)
+                        {
+                            ParticleManager.NewParticle(player.Center, RedeHelper.Spread(10), new SpiritParticle(), Color.White, 2);
+
+                            int dust2 = Dust.NewDust(NPC.position + NPC.velocity, NPC.width, NPC.height, DustID.DungeonSpirit,
+                                NPC.velocity.X * 0.5f, NPC.velocity.Y * 0.5f, Scale: 2);
+                            Main.dust[dust2].velocity *= 10f;
+                            Main.dust[dust2].noGravity = true;
+                        }
+                        NPC.active = false;
+                    }
+                    break;
+            }
+        }
+        public override bool? CanHitNPC(NPC target) => false;
+    }
+}
