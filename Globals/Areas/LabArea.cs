ded53c583e701e66f0024712481eb454faabcf3c
Hallam9K <32880148+Hallam9K@users.noreply.github.com>
2022-07-09 21:44:55 +0100

    lab spawn multiplayer fix? idk

--- Globals/Areas/LabArea.cs
+++ Globals/Areas/LabArea.cs
@@ -32,40 +32,40 @@ namespace Redemption.Globals
 
             Vector2 ToasterPos = new(((RedeGen.LabVector.X + 84) * 16) + 14, (RedeGen.LabVector.Y + 42) * 16);
             if (!Terraria.NPC.AnyNPCs(ModContent.NPCType<JustANormalToaster>()))
-                Terraria.NPC.NewNPC(new EntitySource_SpawnNPC(), (int)ToasterPos.X, (int)ToasterPos.Y, ModContent.NPCType<JustANormalToaster>());
+                RedeHelper.SpawnNPC(new EntitySource_SpawnNPC(), (int)ToasterPos.X, (int)ToasterPos.Y, ModContent.NPCType<JustANormalToaster>());
 
             Vector2 JanitorPos = new((RedeGen.LabVector.X + 173) * 16, (RedeGen.LabVector.Y + 22) * 16);
             if (!Terraria.NPC.AnyNPCs(ModContent.NPCType<JanitorBot_Cleaning>()) && !Terraria.NPC.AnyNPCs(ModContent.NPCType<JanitorBot>())
                 && !RedeBossDowned.downedJanitor)
-                Terraria.NPC.NewNPC(new EntitySource_SpawnNPC(), (int)JanitorPos.X, (int)JanitorPos.Y, ModContent.NPCType<JanitorBot_Cleaning>());
+                RedeHelper.SpawnNPC(new EntitySource_SpawnNPC(), (int)JanitorPos.X, (int)JanitorPos.Y, ModContent.NPCType<JanitorBot_Cleaning>());
 
             Vector2 JanitorNPCPos = new((RedeGen.LabVector.X + 181) * 16, (RedeGen.LabVector.Y + 102) * 16);
             if (!Terraria.NPC.AnyNPCs(ModContent.NPCType<JanitorBot_NPC>()) && RedeBossDowned.downedJanitor)
-                Terraria.NPC.NewNPC(new EntitySource_SpawnNPC(), (int)JanitorNPCPos.X, (int)JanitorNPCPos.Y, ModContent.NPCType<JanitorBot_NPC>());
+                RedeHelper.SpawnNPC(new EntitySource_SpawnNPC(), (int)JanitorNPCPos.X, (int)JanitorNPCPos.Y, ModContent.NPCType<JanitorBot_NPC>());
 
             Vector2 BehemothPos = new(((RedeGen.LabVector.X + 214) * 16) - 4, (RedeGen.LabVector.Y + 45) * 16);
             if (!Terraria.NPC.AnyNPCs(ModContent.NPCType<IrradiatedBehemoth_Inactive>()) && !Terraria.NPC.AnyNPCs(ModContent.NPCType<IrradiatedBehemoth>()) && !RedeBossDowned.downedBehemoth)
-                Terraria.NPC.NewNPC(new EntitySource_SpawnNPC(), (int)BehemothPos.X, (int)BehemothPos.Y, ModContent.NPCType<IrradiatedBehemoth_Inactive>());
+                RedeHelper.SpawnNPC(new EntitySource_SpawnNPC(), (int)BehemothPos.X, (int)BehemothPos.Y, ModContent.NPCType<IrradiatedBehemoth_Inactive>());
 
             Vector2 BlisterfacePos = new(((RedeGen.LabVector.X + 209) * 16) - 4, (RedeGen.LabVector.Y + 191) * 16);
             if (!Terraria.NPC.AnyNPCs(ModContent.NPCType<Blisterface_Inactive>()) && !Terraria.NPC.AnyNPCs(ModContent.NPCType<Blisterface>()) && !RedeBossDowned.downedBlisterface)
-                Terraria.NPC.NewNPC(new EntitySource_SpawnNPC(), (int)BlisterfacePos.X, (int)BlisterfacePos.Y, ModContent.NPCType<Blisterface_Inactive>());
+                RedeHelper.SpawnNPC(new EntitySource_SpawnNPC(), (int)BlisterfacePos.X, (int)BlisterfacePos.Y, ModContent.NPCType<Blisterface_Inactive>());
 
             Vector2 VoltPos = new((RedeGen.LabVector.X + 49) * 16, (RedeGen.LabVector.Y + 122) * 16);
             if (!Terraria.NPC.AnyNPCs(ModContent.NPCType<ProtectorVolt>()) && !Terraria.NPC.AnyNPCs(ModContent.NPCType<ProtectorVolt_Start>()) && !Terraria.NPC.AnyNPCs(ModContent.NPCType<ProtectorVolt_NPC>()))
-                Terraria.NPC.NewNPC(new EntitySource_SpawnNPC(), (int)VoltPos.X, (int)VoltPos.Y, ModContent.NPCType<ProtectorVolt_Start>());
+                RedeHelper.SpawnNPC(new EntitySource_SpawnNPC(), (int)VoltPos.X, (int)VoltPos.Y, ModContent.NPCType<ProtectorVolt_Start>());
 
             Vector2 CraneOperatorPos = new(((RedeGen.LabVector.X + 107) * 16) + 8, (RedeGen.LabVector.Y + 157) * 16);
             if (!Terraria.NPC.AnyNPCs(ModContent.NPCType<CraneOperator>()) && !RedeBossDowned.downedMACE)
-                Terraria.NPC.NewNPC(new EntitySource_SpawnNPC(), (int)CraneOperatorPos.X, (int)CraneOperatorPos.Y, ModContent.NPCType<CraneOperator>());
+                RedeHelper.SpawnNPC(new EntitySource_SpawnNPC(), (int)CraneOperatorPos.X, (int)CraneOperatorPos.Y, ModContent.NPCType<CraneOperator>());
 
             Vector2 MacePos = new(((RedeGen.LabVector.X + 74) * 16) - 8, (RedeGen.LabVector.Y + 169) * 16);
             if (!Terraria.NPC.AnyNPCs(ModContent.NPCType<MACEProject_Off>()) && !Terraria.NPC.AnyNPCs(ModContent.NPCType<MACEProject>()) && !RedeBossDowned.downedMACE)
-                Terraria.NPC.NewNPC(new EntitySource_SpawnNPC(), (int)MacePos.X, (int)MacePos.Y, ModContent.NPCType<MACEProject_Off>());
+                RedeHelper.SpawnNPC(new EntitySource_SpawnNPC(), (int)MacePos.X, (int)MacePos.Y, ModContent.NPCType<MACEProject_Off>());
 
             Vector2 KariPos = new((RedeGen.LabVector.X + 144) * 16, ((RedeGen.LabVector.Y + 193) * 16) + 1);
             if (!Terraria.NPC.AnyNPCs(ModContent.NPCType<PZ>()) && !Terraria.NPC.AnyNPCs(ModContent.NPCType<PZ_Inactive>()) && !RedeBossDowned.downedPZ)
-                Terraria.NPC.NewNPC(new EntitySource_SpawnNPC(), (int)KariPos.X, (int)KariPos.Y, ModContent.NPCType<PZ_Inactive>());
+                RedeHelper.SpawnNPC(new EntitySource_SpawnNPC(), (int)KariPos.X, (int)KariPos.Y, ModContent.NPCType<PZ_Inactive>());
         }
         public override void OnWorldLoad()
         {
