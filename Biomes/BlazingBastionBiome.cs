396b8f5c934e62f5b4eb816c9d7b249c03161e15
Hallam9K <32880148+Hallam9K@users.noreply.github.com>
2022-07-11 16:42:00 +0100

    Update BlazingBastionBiome.cs

--- Biomes/BlazingBastionBiome.cs
+++ Biomes/BlazingBastionBiome.cs
@@ -1,6 +1,7 @@
 using Microsoft.Xna.Framework;
 using Redemption.Globals;
 using Terraria;
+using Terraria.ID;
 using Terraria.ModLoader;
 namespace Redemption.Biomes
 {
@@ -9,7 +10,8 @@ namespace Redemption.Biomes
         public override string BestiaryIcon => base.BestiaryIcon;
         public override string BackgroundPath => base.BackgroundPath;
         public override Color? BackgroundColor => base.BackgroundColor;
-        public override SceneEffectPriority Priority => SceneEffectPriority.None;
+        public override int Music => MusicID.OtherworldlyUnderworld;
+        public override SceneEffectPriority Priority => SceneEffectPriority.Environment;
         public override void SetStaticDefaults()
         {
             DisplayName.SetDefault("Blazing Bastion");
