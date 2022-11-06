59eb87b5a4774bce69de77678a131f9df2f7334c
Hallam9K <32880148+Hallam9K@users.noreply.github.com>
2022-11-06 15:24:58 +0000

    Update AncientAutoPaintingTile.cs

--- Tiles/Furniture/Misc/AncientAutoPaintingTile.cs
+++ Tiles/Furniture/Misc/AncientAutoPaintingTile.cs
@@ -1,5 +1,5 @@
 using Microsoft.Xna.Framework;
-using Redemption.Items.Placeable.Trophies;
+using Redemption.Items.Placeable.Furniture.Misc;
 using Terraria;
 using Terraria.DataStructures;
 using Terraria.ID;
