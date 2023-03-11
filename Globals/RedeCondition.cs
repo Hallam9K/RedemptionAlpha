d831d483c745edaf3f5c69b865364b91989d74d1
Hallam9K <32880148+Hallam9K@users.noreply.github.com>
2023-03-11 22:23:15 +0000

    Update RedeCondition.cs

--- Globals/RedeCondition.cs
+++ Globals/RedeCondition.cs
@@ -9,36 +9,36 @@ using Redemption.NPCs.Bosses.FowlEmperor;
 namespace Redemption.Globals
 {
     public class DecapitationCondition : IItemDropRuleCondition
-	{
-		public bool CanDrop(DropAttemptInfo info)
-		{
-			if (!info.IsInSimulation && (NPCLists.SkeletonHumanoid.Contains(info.npc.type) || NPCLists.Humanoid.Contains(info.npc.type)))
-			{
-				return info.npc.Redemption().decapitated;
-			}
-			return false;
-		}
-		public bool CanShowItemDropInUI() => false;
-		public string GetConditionDescription() => "Drops when decapitated";
-	}
-	public class LostSoulCondition : IItemDropRuleCondition
-	{
-		public bool CanDrop(DropAttemptInfo info) => false;
-		public bool CanShowItemDropInUI() => true;
-		public string GetConditionDescription() => "Appears as an NPC";
-	}
-	public class OnFireCondition : IItemDropRuleCondition
-	{
-		public bool CanDrop(DropAttemptInfo info) => false;
-		public bool CanShowItemDropInUI() => true;
-		public string GetConditionDescription() => "Dropped while on fire";
-	}
-	public class TeddyCondition : IItemDropRuleCondition
-	{
-		public bool CanDrop(DropAttemptInfo info)
-		{
-			if (!info.IsInSimulation && info.npc.type == ModContent.NPCType<SkullDigger>() && !(info.npc.ModNPC as SkullDigger).KeeperSpawn)
-				return true;
+    {
+        public bool CanDrop(DropAttemptInfo info)
+        {
+            if (!info.IsInSimulation && (NPCLists.SkeletonHumanoid.Contains(info.npc.type) || NPCLists.Humanoid.Contains(info.npc.type)))
+            {
+                return info.npc.Redemption().decapitated;
+            }
+            return false;
+        }
+        public bool CanShowItemDropInUI() => false;
+        public string GetConditionDescription() => "Drops when decapitated";
+    }
+    public class LostSoulCondition : IItemDropRuleCondition
+    {
+        public bool CanDrop(DropAttemptInfo info) => false;
+        public bool CanShowItemDropInUI() => true;
+        public string GetConditionDescription() => "Appears as an NPC";
+    }
+    public class OnFireCondition : IItemDropRuleCondition
+    {
+        public bool CanDrop(DropAttemptInfo info) => false;
+        public bool CanShowItemDropInUI() => true;
+        public string GetConditionDescription() => "Dropped while on fire";
+    }
+    public class TeddyCondition : IItemDropRuleCondition
+    {
+        public bool CanDrop(DropAttemptInfo info)
+        {
+            if (!info.IsInSimulation && info.npc.type == ModContent.NPCType<SkullDigger>() && !(info.npc.ModNPC as SkullDigger).KeeperSpawn)
+                return true;
 
             return false;
         }
