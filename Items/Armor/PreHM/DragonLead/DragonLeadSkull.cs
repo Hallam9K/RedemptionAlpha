2fc0ac8bd6aa4da15308d221a5e5000ee7cde0e7
polaritz <97950849+Polaritz@users.noreply.github.com>
2023-06-11 19:58:22 +0900

    Update DragonLeadSkull.cs

--- Items/Armor/PreHM/DragonLead/DragonLeadSkull.cs
+++ Items/Armor/PreHM/DragonLead/DragonLeadSkull.cs
@@ -1,23 +1,6 @@
 using Terraria;
 using Terraria.ModLoader;
 using Terraria.ID;
-using Redemption.Items.Materials.PreHM;
-using System.Collections.Generic;
-using Microsoft.Xna.Framework;
-using Redemption.BaseExtension;
-using Redemption.Globals;
-
-namespace Redemption.Items.Armor.PreHM.DragonLead
-{
-    [AutoloadEquip(EquipType.Head)]
-    public class DragonLeadSkull : ModItem
-    {
-        public override void SetStaticDefaults()
-        {
-            // DisplayName.SetDefault("Dragon-Lead Skull");
-            // Tooltip.SetDefault("7% increased damage");using Terraria;
-using Terraria.ModLoader;
-using Terraria.ID;
 using Terraria.Localization;
 using Redemption.Items.Materials.PreHM;
 using System.Collections.Generic;
@@ -89,74 +72,6 @@ namespace Redemption.Items.Armor.PreHM.DragonLead
             else
             {
                 TooltipLine line = new(Mod, "HoldShift", Language.GetTextValue("Mods.Redemption.SpecialTooltips.Viewer"))
-                {
-                    OverrideColor = Color.Gray,
-                };
-                tooltips.Add(line);
-            }
-        }
-    }
-}
-            ArmorIDs.Head.Sets.DrawHead[EquipLoader.GetEquipSlot(Mod, Name, EquipType.Head)] = false;
-
-            Item.ResearchUnlockCount = 1;
-        }
-
-        public override void SetDefaults()
-        {
-            Item.width = 24;
-            Item.height = 28;
-            Item.sellPrice(silver: 60);
-            Item.rare = ItemRarityID.Orange;
-            Item.defense = 6;
-        }
-
-        public override bool IsArmorSet(Item head, Item body, Item legs)
-        {
-            return body.type == ModContent.ItemType<DragonLeadRibplate>() && legs.type == ModContent.ItemType<DragonLeadGreaves>();
-        }
-
-        public override void UpdateEquip(Player player)
-        {
-            player.GetDamage(DamageClass.Generic) += 0.07f;
-        }
-
-        public override void UpdateArmorSet(Player player)
-        {
-            player.setBonus = "20% increased " + ElementID.IceS + " elemental resistance\n" +
-                "33% chance for weapons to inflict Dragonblaze\n" +
-                "100% chance for all Dragon-Lead weapons to inflict Dragonblaze";
-            player.RedemptionPlayerBuff().ElementalResistance[ElementID.Ice] += 0.2f;
-            player.RedemptionPlayerBuff().dragonLeadBonus = true;
-            player.RedemptionPlayerBuff().MetalSet = true;
-        }
-
-        public override void AddRecipes()
-        {
-            CreateRecipe()
-                .AddIngredient(ModContent.ItemType<DragonLeadAlloy>(), 10)
-                .AddIngredient(ItemID.Bone, 2)
-                .AddTile(TileID.Anvils)
-                .Register();
-        }
-
-        public override void ModifyTooltips(List<TooltipLine> tooltips)
-        {
-            if (Main.keyState.PressingShift())
-            {
-                TooltipLine line = new(Mod, "Lore",
-                    "'A horned helmet forged from melted dragon bone and metal, made to look like a dragon's skull. It is said\n" +
-                    "to be used by the ancient warlords of Dragonrest.\n" +
-                    "The warlords were famous dragon slayers who used the bones of their victims for weaponry and armour,\n" +
-                    "nearly bringing the dragons to extinction. That was until every single one was wiped out by Goliathon, the Dragon God.'")
-                {
-                    OverrideColor = Color.LightGray
-                };
-                tooltips.Add(line);
-            }
-            else
-            {
-                TooltipLine line = new(Mod, "HoldShift", "Hold [Shift] to view lore")
                 {
                     OverrideColor = Color.Gray,
                 };
