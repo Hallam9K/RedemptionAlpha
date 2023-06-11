fc25f7697125adc6a5bf96ad3dc25c20d7bc60d3
polaritz <97950849+Polaritz@users.noreply.github.com>
2023-06-11 13:49:27 +0900

    Update DragonLeadRibplate.cs

--- Items/Armor/PreHM/DragonLead/DragonLeadRibplate.cs
+++ Items/Armor/PreHM/DragonLead/DragonLeadRibplate.cs
@@ -12,7 +12,81 @@ namespace Redemption.Items.Armor.PreHM.DragonLead
     public class DragonLeadRibplate : ModItem
     {
         public override void SetStaticDefaults()
+        {using Microsoft.Xna.Framework;
+using Redemption.Buffs.Debuffs;
+using Redemption.Items.Materials.PreHM;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace Redemption.Items.Armor.PreHM.DragonLead
+{
+    [AutoloadEquip(EquipType.Body)]
+    public class DragonLeadRibplate : ModItem
+    {
+        public override void SetStaticDefaults()
         {
+            // DisplayName.SetDefault("Dragon-Lead Ribplate");
+            /* Tooltip.SetDefault("7% increased damage\n" +
+                "Immunity to most ice-related debuffs"); */
+            ArmorIDs.Body.Sets.IncludedCapeBack[EquipLoader.GetEquipSlot(Mod, Name, EquipType.Body)] = Redemption.dragonLeadCapeID;
+            ArmorIDs.Body.Sets.IncludedCapeBackFemale[EquipLoader.GetEquipSlot(Mod, Name, EquipType.Body)] = Redemption.dragonLeadCapeID;
+
+            Item.ResearchUnlockCount = 1;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 38;
+            Item.height = 28;
+            Item.sellPrice(0, 0, 65);
+            Item.rare = ItemRarityID.Orange;
+            Item.defense = 7;
+        }
+
+        public override void UpdateEquip(Player player)
+        {
+            player.GetDamage(DamageClass.Generic) += .07f;
+            player.buffImmune[BuffID.Frozen] = true;
+            player.buffImmune[BuffID.Chilled] = true;
+            player.buffImmune[BuffID.Frostburn] = true;
+            player.buffImmune[BuffID.Frostburn2] = true;
+            player.buffImmune[BuffID.Wet] = true;
+            player.buffImmune[ModContent.BuffType<PureChillDebuff>()] = true;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ModContent.ItemType<DragonLeadAlloy>(), 20)
+                .AddIngredient(ItemID.Bone, 4)
+                .AddTile(TileID.Anvils)
+                .Register();
+        }
+
+        public override void ModifyTooltips(List<TooltipLine> tooltips)
+        {
+            if (Main.keyState.PressingShift())
+            {
+                TooltipLine line = new(Mod, "Lore", Language.GetTextValue("Mods.Redemption.SpecialTooltips.DragonLead.DragonLeadRibplate"))
+                {
+                    OverrideColor = Color.LightGray
+                };
+                tooltips.Add(line);
+            }
+            else
+            {
+                TooltipLine line = new(Mod, "HoldShift", Language.GetTextValue("Mods.Redemption.SpecialTooltips.Viewer"))
+                {
+                    OverrideColor = Color.Gray,
+                };
+                tooltips.Add(line);
+            }
+        }
+    }
+}
             // DisplayName.SetDefault("Dragon-Lead Ribplate");
             /* Tooltip.SetDefault("7% increased damage\n" +
                 "Immunity to most ice-related debuffs"); */
