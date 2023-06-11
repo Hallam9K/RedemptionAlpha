2476a870e461e38117bf765b7239a59f08fd6bab
polaritz <97950849+Polaritz@users.noreply.github.com>
2023-06-11 13:48:33 +0900

    Update CommonGuardGreaves.cs

--- Items/Armor/PreHM/CommonGuard/CommonGuardGreaves.cs
+++ Items/Armor/PreHM/CommonGuard/CommonGuardGreaves.cs
@@ -14,7 +14,70 @@ namespace Redemption.Items.Armor.PreHM.CommonGuard
 		{
 			// Tooltip.SetDefault("7% increased melee speed");
 
+			Item.ResearchUnlockCount = 1;using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Terraria.Localization;
+using System.Collections.Generic;
+using Redemption.Items.Materials.PreHM;
+using Microsoft.Xna.Framework;
+
+namespace Redemption.Items.Armor.PreHM.CommonGuard
+{
+    [AutoloadEquip(EquipType.Legs)]
+	public class CommonGuardGreaves : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			// Tooltip.SetDefault("7% increased melee speed");
+
 			Item.ResearchUnlockCount = 1;
+		}
+
+		public override void SetDefaults()
+		{
+			Item.width = 22;
+			Item.height = 18;
+			Item.sellPrice(silver: 30);
+			Item.rare = ItemRarityID.Green;
+			Item.defense = 3;
+		}
+
+		public override void UpdateEquip(Player player)
+		{
+			player.GetAttackSpeed(DamageClass.Melee) += .07f;
+		}
+
+		public override void AddRecipes()
+		{
+            CreateRecipe()
+                .AddIngredient(ModContent.ItemType<GraveSteelAlloy>(), 15)
+                .AddIngredient(ItemID.Silk, 4)
+                .AddTile(TileID.Anvils)
+                .Register();
+        }
+
+        public override void ModifyTooltips(List<TooltipLine> tooltips)
+        {
+            if (Main.keyState.PressingShift())
+            {
+                TooltipLine line = new(Mod, "Lore", Language.GetTextValue("Mods.Redemption.SpecialTooltips.CommonGuard.CommonGuardGreaves"))
+                {
+                    OverrideColor = Color.LightGray
+                };
+                tooltips.Add(line);
+            }
+            else
+            {
+                TooltipLine line = new(Mod, "HoldShift", Language.GetTextValue("Mods.Redemption.SpecialTooltips.Viewer"))
+                {
+                    OverrideColor = Color.Gray,
+                };
+                tooltips.Add(line);
+            }
+        }
+    }
+}
 		}
 
 		public override void SetDefaults()
