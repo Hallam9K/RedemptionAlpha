19423099218b533d9cb08d838da91ef4e6cb727b
polaritz <97950849+Polaritz@users.noreply.github.com>
2023-06-11 13:45:02 +0900

    Update PureIronGreaves.cs

--- Items/Armor/PreHM/PureIron/PureIronGreaves.cs
+++ Items/Armor/PreHM/PureIron/PureIronGreaves.cs
@@ -51,7 +51,71 @@ namespace Redemption.Items.Armor.PreHM.PureIron
                     "however the Iron Realm's warriors have been trained to resist such harsh temperatures.\n\n" +
                     "The Warriors of the Iron Realm are Gathuram's main military force,\n" +
                     "with units spanning all across the domain.'")
+                {using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Terraria.Localization;
+using System.Collections.Generic;
+using Redemption.Items.Materials.PreHM;
+using Microsoft.Xna.Framework;
+
+namespace Redemption.Items.Armor.PreHM.PureIron
+{
+    [AutoloadEquip(EquipType.Legs)]
+    public class PureIronGreaves : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            // DisplayName.SetDefault("Pure-Iron Greaves");
+            // Tooltip.SetDefault("8% increased critical strike chance");
+
+            Item.ResearchUnlockCount = 1;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 22;
+            Item.height = 16;
+            Item.sellPrice(silver: 50);
+            Item.rare = ItemRarityID.Orange;
+            Item.defense = 8;
+        }
+
+        public override void UpdateEquip(Player player)
+        {
+            player.GetCritChance(DamageClass.Generic) += 8;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ModContent.ItemType<PureIronAlloy>(), 15)
+                .AddIngredient(ItemID.Leather, 2)
+                .AddTile(TileID.Anvils)
+                .Register();
+        }
+
+        public override void ModifyTooltips(List<TooltipLine> tooltips)
+        {
+            if (Main.keyState.PressingShift())
+            {
+                TooltipLine line = new(Mod, "Lore", Language.GetTextValue("Mods.Redemption.SpecialTooltips.PureIron.PureIronGreaves"))
                 {
+                    OverrideColor = Color.LightGray
+                };
+                tooltips.Add(line);
+            }
+            else
+            {
+                TooltipLine line = new(Mod, "HoldShift", Language.GetTextValue("Mods.Redemption.SpecialTooltips.Viewer"))
+                {
+                    OverrideColor = Color.Gray,
+                };
+                tooltips.Add(line);
+            }
+        }
+    }
+}
                     OverrideColor = Color.LightGray
                 };
                 tooltips.Add(line);
