fca93b9df850d01c67f06fa43df29f5d0bcb8598
polaritz <97950849+Polaritz@users.noreply.github.com>
2023-06-11 13:47:42 +0900

    Update CommonGuardHelm2.cs

--- Items/Armor/PreHM/CommonGuard/CommonGuardHelm2.cs
+++ Items/Armor/PreHM/CommonGuard/CommonGuardHelm2.cs
@@ -13,7 +13,96 @@ namespace Redemption.Items.Armor.PreHM.CommonGuard
     public class CommonGuardHelm2 : ModItem
     {
         public override void SetStaticDefaults()
+        {using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Terraria.Localization;
+using Redemption.Items.Materials.PreHM;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using System;
+using Redemption.BaseExtension;
+
+namespace Redemption.Items.Armor.PreHM.CommonGuard
+{
+    [AutoloadEquip(EquipType.Head)]
+    public class CommonGuardHelm2 : ModItem
+    {
+        public override void SetStaticDefaults()
         {
+            // DisplayName.SetDefault("Common Guard Bucket Helm");
+            // Tooltip.SetDefault("+1 increased melee damage");
+            ArmorIDs.Head.Sets.DrawHead[EquipLoader.GetEquipSlot(Mod, Name, EquipType.Head)] = false;
+
+            Item.ResearchUnlockCount = 1;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 24;
+            Item.height = 26;
+            Item.sellPrice(silver: 30);
+            Item.rare = ItemRarityID.Green;
+            Item.defense = 5;
+        }
+
+        public override bool IsArmorSet(Item head, Item body, Item legs)
+        {
+            return body.type == ModContent.ItemType<CommonGuardPlateMail>() && legs.type == ModContent.ItemType<CommonGuardGreaves>();
+        }
+
+        public override void UpdateEquip(Player player)
+        {
+            player.GetDamage(DamageClass.Melee).Flat += 1;
+        }
+
+        public override void UpdateArmorSet(Player player)
+        {
+            player.setBonus = Language.GetTextValue("Mods.Redemption.GenericTooltips.ArmorSetBonus.CommonGuardHelm2");
+            player.endurance += .06f;
+            player.RedemptionPlayerBuff().MetalSet = true;
+
+            if (Main.rand.NextBool(10) && Math.Abs(player.velocity.X) + Math.Abs(player.velocity.Y) > 1f && !player.rocketFrame)
+            {
+                int index = Dust.NewDust(new Vector2(player.position.X - player.velocity.X * 2f, player.position.Y - 2f - player.velocity.Y * 2f), player.width, player.height,
+                    DustID.Web);
+                Main.dust[index].noGravity = true;
+                Dust dust = Main.dust[index];
+                dust.velocity -= player.velocity * 0.5f;
+            }
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ModContent.ItemType<GraveSteelAlloy>(), 10)
+                .AddIngredient(ItemID.Silk, 2)
+                .AddTile(TileID.Anvils)
+                .Register();
+        }
+
+        public override void ModifyTooltips(List<TooltipLine> tooltips)
+        {
+            if (Main.keyState.PressingShift())
+            {
+                TooltipLine line = new(Mod, "Lore", Language.GetTextValue("Mods.Redemption.SpecialTooltips.CommonGuard.CommonGuardHelm2"))
+                {
+                    OverrideColor = Color.LightGray
+                };
+                tooltips.Add(line);
+            }
+            else
+            {
+                TooltipLine line = new(Mod, "HoldShift", Language.GetTextValue("Mods.Redemption.SpecialTooltips.Viewer"))
+                {
+                    OverrideColor = Color.Gray,
+                };
+                tooltips.Add(line);
+            }
+        }
+    }
+}
+
             // DisplayName.SetDefault("Common Guard Bucket Helm");
             // Tooltip.SetDefault("+1 increased melee damage");
             ArmorIDs.Head.Sets.DrawHead[EquipLoader.GetEquipSlot(Mod, Name, EquipType.Head)] = false;
