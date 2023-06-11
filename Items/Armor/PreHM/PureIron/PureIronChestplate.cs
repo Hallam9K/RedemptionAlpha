bb93f11c510763c8b8d25ee0ddb4e809813a83ec
polaritz <97950849+Polaritz@users.noreply.github.com>
2023-06-11 13:45:55 +0900

    Update PureIronChestplate.cs

--- Items/Armor/PreHM/PureIron/PureIronChestplate.cs
+++ Items/Armor/PreHM/PureIron/PureIronChestplate.cs
@@ -9,7 +9,152 @@ namespace Redemption.Items.Armor.PreHM.PureIron
 {
     [AutoloadEquip(EquipType.Body)]
     public class PureIronChestplate : ModItem
+    {using Microsoft.Xna.Framework;
+using Redemption.Buffs.Debuffs;
+using Redemption.Items.Materials.PreHM;using Microsoft.Xna.Framework;
+using Redemption.Items.Materials.PreHM;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.Localization;
+
+namespace Redemption.Items.Armor.PreHM.PureIron
+{
+    [AutoloadEquip(EquipType.Body)]
+    public class PureIronChestplate : ModItem
     {
+        public override void SetStaticDefaults()
+        {
+            // DisplayName.SetDefault("Pure-Iron Chestplate");
+            /* Tooltip.SetDefault("7% increased damage\n" +
+                "Immunity to most fire-related debuffs"); */
+
+            Item.ResearchUnlockCount = 1;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 38;
+            Item.height = 30;
+            Item.sellPrice(0, 0, 65);
+            Item.rare = ItemRarityID.Orange;
+            Item.defense = 9;
+        }
+
+        public override void UpdateEquip(Player player)
+        {
+            player.GetDamage(DamageClass.Generic) += .07f;
+            player.buffImmune[BuffID.OnFire] = true;
+            player.buffImmune[BuffID.OnFire3] = true;
+            player.buffImmune[BuffID.CursedInferno] = true;
+            player.buffImmune[BuffID.Burning] = true;
+            player.buffImmune[BuffID.Frostburn] = true;
+            player.buffImmune[BuffID.Frostburn2] = true;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ModContent.ItemType<PureIronAlloy>(), 20)
+                .AddIngredient(ItemID.Leather, 4)
+                .AddTile(TileID.Anvils)
+                .Register();
+        }
+
+        public override void ModifyTooltips(List<TooltipLine> tooltips)
+        {
+            if (Main.keyState.PressingShift())
+            {
+                TooltipLine line = new(Mod, "Lore", Language.GetTextValue("Mods.Redemption.SpecialTooltips.PureIron.PureIronChestplate"))
+                {
+                    OverrideColor = Color.LightGray
+                };
+                tooltips.Add(line);
+            }
+            else
+            {
+                TooltipLine line = new(Mod, "HoldShift", Language.GetTextValue("Mods.Redemption.SpecialTooltips.Viewer"))
+                {
+                    OverrideColor = Color.Gray,
+                };
+                tooltips.Add(line);
+            }
+        }
+    }
+}
+using System.Collections.Generic;
+using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace Redemption.Items.Armor.PreHM.DragonLead
+{
+    [AutoloadEquip(EquipType.Body)]
+    public class DragonLeadRibplate : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            // DisplayName.SetDefault("Dragon-Lead Ribplate");
+            /* Tooltip.SetDefault("7% increased damage\n" +
+                "Immunity to most ice-related debuffs"); */
+            ArmorIDs.Body.Sets.IncludedCapeBack[EquipLoader.GetEquipSlot(Mod, Name, EquipType.Body)] = Redemption.dragonLeadCapeID;
+            ArmorIDs.Body.Sets.IncludedCapeBackFemale[EquipLoader.GetEquipSlot(Mod, Name, EquipType.Body)] = Redemption.dragonLeadCapeID;
+
+            Item.ResearchUnlockCount = 1;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 38;
+            Item.height = 28;
+            Item.sellPrice(0, 0, 65);
+            Item.rare = ItemRarityID.Orange;
+            Item.defense = 7;
+        }
+
+        public override void UpdateEquip(Player player)
+        {
+            player.GetDamage(DamageClass.Generic) += .07f;
+            player.buffImmune[BuffID.Frozen] = true;
+            player.buffImmune[BuffID.Chilled] = true;
+            player.buffImmune[BuffID.Frostburn] = true;
+            player.buffImmune[BuffID.Frostburn2] = true;
+            player.buffImmune[BuffID.Wet] = true;
+            player.buffImmune[ModContent.BuffType<PureChillDebuff>()] = true;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ModContent.ItemType<DragonLeadAlloy>(), 20)
+                .AddIngredient(ItemID.Bone, 4)
+                .AddTile(TileID.Anvils)
+                .Register();
+        }
+
+        public override void ModifyTooltips(List<TooltipLine> tooltips)
+        {
+            if (Main.keyState.PressingShift())
+            {
+                TooltipLine line = new(Mod, "Lore", Language.GetTextValue("Mods.Redemption.SpecialTooltips.DragonLead.DragonLeadRibplate"))
+                {
+                    OverrideColor = Color.LightGray
+                };
+                tooltips.Add(line);
+            }
+            else
+            {
+                TooltipLine line = new(Mod, "HoldShift", Language.GetTextValue("Mods.Redemption.SpecialTooltips.Viewer"))
+                {
+                    OverrideColor = Color.Gray,
+                };
+                tooltips.Add(line);
+            }
+        }
+    }
+}
         public override void SetStaticDefaults()
         {
             // DisplayName.SetDefault("Pure-Iron Chestplate");
