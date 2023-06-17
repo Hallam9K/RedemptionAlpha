96e3bafea36562416710b22a7091a87b0afd898d
Hallam9K <32880148+Hallam9K@users.noreply.github.com>
2023-06-17 12:00:25 +0100

    Update EggCracker.cs

--- NPCs/Minibosses/FowlEmperor/EggCracker.cs
+++ NPCs/Minibosses/FowlEmperor/EggCracker.cs
@@ -1,3 +1,6 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Redemption.Base;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
@@ -44,6 +47,41 @@ namespace Redemption.NPCs.Minibosses.FowlEmperor
                         player.inventory[egg] = new Item();
                 }
             }
+        }
+        private float glowRot = 0;
+        public override void PostUpdate()
+        {
+            glowRot += 0.03f;
+        }
+        public override bool PreDrawInWorld(SpriteBatch spriteBatch, Color lightColor, Color alphaColor, ref float rotation, ref float scale, int whoAmI)
+        {
+            Texture2D glow = ModContent.Request<Texture2D>("Redemption/Textures/WhiteFlare").Value;
+            Vector2 origin = new(glow.Width / 2, glow.Height / 2);
+
+            spriteBatch.End();
+            spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.Additive, Main.DefaultSamplerState, DepthStencilState.None, RasterizerState.CullCounterClockwise, null, Main.GameViewMatrix.TransformationMatrix);
+
+            spriteBatch.Draw(glow, Item.Center - Main.screenPosition, new Rectangle(0, 0, glow.Width, glow.Height), Color.IndianRed, glowRot, origin, scale, SpriteEffects.None, 0f);
+            spriteBatch.Draw(glow, Item.Center - Main.screenPosition, new Rectangle(0, 0, glow.Width, glow.Height), Color.IndianRed, -glowRot, origin, scale, SpriteEffects.None, 0f);
+
+            spriteBatch.End();
+            spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, Main.DefaultSamplerState, DepthStencilState.None, RasterizerState.CullCounterClockwise, null, Main.GameViewMatrix.TransformationMatrix);
+            return true;
+        }
+        public override bool PreDrawInInventory(SpriteBatch spriteBatch, Vector2 position, Rectangle frame, Color drawColor, Color itemColor, Vector2 origin, float scale)
+        {
+            Texture2D glow = ModContent.Request<Texture2D>("Redemption/Textures/WhiteFlare").Value;
+            Vector2 origin2 = new(glow.Width / 2, glow.Height / 2);
+
+            spriteBatch.End();
+            spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.Additive, SamplerState.LinearClamp, DepthStencilState.Default, RasterizerState.CullNone, null, Main.UIScaleMatrix);
+
+            spriteBatch.Draw(glow, position, new Rectangle(0, 0, glow.Width, glow.Height), Color.IndianRed, glowRot, origin2, scale, SpriteEffects.None, 0f);
+            spriteBatch.Draw(glow, position, new Rectangle(0, 0, glow.Width, glow.Height), Color.IndianRed, -glowRot, origin2, scale, SpriteEffects.None, 0f);
+
+            spriteBatch.End();
+            spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.LinearClamp, DepthStencilState.Default, RasterizerState.CullNone, null, Main.UIScaleMatrix);
+            return true;
         }
         public override void Update(ref float gravity, ref float maxFallSpeed)
         {
